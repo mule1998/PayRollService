5be8173fa65b1c632b104bc3bcbb94ca4f47456f
Shubham Mule <shubhambmule@gmail.com>
2021-11-12 17:26:36 +0530

    Added : Table

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -7,3 +7,15 @@
 
 CREATE DATABASE payroll_service;
 USE payroll_service;
+
+
+---------UC2-----------
+----Retrieve PayRoll----
+
+
+CREATE TABLE employee_payroll(
+ID int not null,
+name varchar(30) not null,
+salary money not null,
+startdate date not null
+);
