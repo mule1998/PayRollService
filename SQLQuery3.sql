a1c4743a01ab73b390d356ee41ecbd974cbef458
Shubham Mule <shubhambmule@gmail.com>
2021-11-12 17:15:33 +0530

    Added : Database

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -1,1 +1,9 @@
 -------Welcome to PayRollService----------
+
+
+----------UC1----------------
+-------Create DataBase PayRollService------
+
+
+CREATE DATABASE payroll_service;
+USE payroll_service;
